1bfe10e26a91d85189a43eeb2784cb9053b5d274
Katherine Duchesneau <Katherine.duchesneau@gmail.com>
2018-04-05 17:00:01 -0400

    Update ModifySRAtoILLUMINA.py

--- ModifySRAtoILLUMINA.py
+++ ModifySRAtoILLUMINA.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import os
-os.chdir("/global/home/sa104007/EndophytePRJ") 
-# setting my working directory to facilitate pulling files in
+
+import sys
+inFile = sys.argv[1]
+outFile = sys.argv[2]
 
 import re
 # importing the regex library
-
-with open ("toy.fastq", 'r' ) as ifp: #WE NEED TO FIND A WAY TO RUN THROUGH ALL OUR SRA?
-    with open("toynew.fasta", "w") as ofp: #and then write in file output.txt (call it ofp)
+with open (inFile, 'r' ) as ifp: #WE NEED TO FIND A WAY TO RUN THROUGH ALL OUR SRA?
+    with open(outFile, "w") as ofp: #and then write in file output.txt (call it ofp)
         for line in ifp: # each line (call it line)
             if re.search(r'^@',line): # if you find a line that starts with @
                 ofp.write(re.sub(r'^@([A-Z,1-9]+)\.(\d+)\s.+\n', '@Inst:\\1:1:1:1:1:1 \\2:Y:1:1\n',line)) # select only the portion after the space until the end of line
@@ -16,4 +16,3 @@ with open ("toy.fastq", 'r' ) as ifp: #WE NEED TO FIND A WAY TO RUN THROUGH ALL 
             else:
                 ofp.write('') # delete everything else
                 
-quit()
