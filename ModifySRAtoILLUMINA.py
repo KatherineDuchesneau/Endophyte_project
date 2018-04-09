768badc64b5a4399d4ea1eb0c18d4ce566508654
Katherine Duchesneau <Katherine.duchesneau@gmail.com>
2018-04-09 13:35:26 -0400

    Update ModifySRAtoILLUMINA.py

--- ModifySRAtoILLUMINA.py
+++ ModifySRAtoILLUMINA.py
@@ -11,7 +11,7 @@ with open (inFile, 'r' ) as ifp: #WE NEED TO FIND A WAY TO RUN THROUGH ALL OUR S
     with open(outFile, "w") as ofp: #and then write in file output.txt (call it ofp)
         for line in ifp: # each line (call it line)
             if re.search(r'^@',line): # if you find a line that starts with @
-                ofp.write(re.sub(r'^@([A-Z,1-9]+)\.(\d+)\s.+$', '@Inst:\\1:1:1:1:1:1 \\2:Y:1:1\n',line)) # select only the portion after the space until the end of line
+                ofp.write(re.sub(r'^@([A-Z,1-9]+)\.(\d+)\s.+\n', '@Inst:\\1:1:1:1:1:1 \\2:Y:1:1\n',line)) # select only the portion after the space until the end of line
             if re.search(r'^[A,T,G,C]+$',line): # if you find a line that starts with BP
                   ofp.write(re.sub(r'([A,T,G,C]+)', '\\1',line)) # select it (in case the definition is multiline... if there are mistakes they will be taken care of later)             )
             else:
