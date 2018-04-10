d38a938b24f48453f3bacbfaa2393d0070d756a9
hannahdriver <36014350+hannahdriver@users.noreply.github.com>
2018-04-09 21:21:01 -0400

    Update Trimmomatic_Job.sh

--- bin/Trimmomatic_Job.sh
+++ bin/Trimmomatic_Job.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=Trimmomatic
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=h_driver23@hotmail.com
-#SBATCH --error=Trimmomatic_3.err
+#SBATCH --error=Trimmomatic_error.err
 
 module load trimmomatic
 
@@ -12,20 +12,41 @@ for file in ./Zea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.j
 
 for file in ./Vitis/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
 
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+for file in ./Solanum/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Santiria/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Rothmannia/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Pylaisiella/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Populus/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Pinus_flexilis/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Pinus/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Paeonia_suffruticosa/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Paeonia_rockii/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Oryza/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Orchid/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Microdesmis/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Grimmia/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Espeletia/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Cratoneuron/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Chladophora/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+
+for file in ./Bryum/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
 
 for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
 
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
+for file in ./Brassica_napus/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
 
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
-
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
-
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
-
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
-
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
-
-for file in ./Brassica_oleracea/*.fastq.gz; do java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.36.jar SE -phred33 ./$file ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36; done
