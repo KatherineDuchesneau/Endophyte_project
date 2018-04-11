0b7fdfbfabc7456d88e58a1229a688e84832ab82
hannahdriver <36014350+hannahdriver@users.noreply.github.com>
2018-04-11 14:48:55 -0400

    ERR & SRR

--- ModifySRAtoILLUMINA.py
+++ ModifySRAtoILLUMINA.py
@@ -9,7 +9,7 @@ import re
 with open (inFile, 'r' ) as ifp: #WE NEED TO FIND A WAY TO RUN THROUGH ALL OUR SRA?
     with open(outFile, "w") as ofp: #and then write in file output.txt (call it ofp)
         for line in ifp: # each line (call it line)
-            if re.search(r'^@[E,S]',line): # if you find a line that starts with @
+            if re.search(r'^@[ERR,SRR]',line): # if you find a line that starts with @
                 ofp.write(re.sub(r'^@([A-Z,0-9]+)\.(\d+)\s.+\n', '>\\1_\\2\n',line)) # select only the portion after the space until the end of line
             if re.search(r'^[A,T,G,C,N]+$',line): # if you find a line that starts with BP
                 ofp.write(re.sub(r'([A,T,G,C,N]+)', '\\1',line))
