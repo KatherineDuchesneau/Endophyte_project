2a4d8d103f35777cdc57e40729f1cc3a968ca65d
hannahdriver <36014350+hannahdriver@users.noreply.github.com>
2018-04-13 15:00:48 -0400

    comments

--- fasta_make.sh
+++ fasta_make.sh
@@ -8,15 +8,19 @@
 
 module load python/3.6.3
 
+# unzip all zipped fastq files in each of the species directories
 gunzip ./*/*.fastq.gz
 
+# employ regex script ("ModifySRAtoILLUMINA") on all fastq files in each species directory to convert them to fasta format (with extensions ".fastq.fasta")
 for i in ./*/*.fastq
 do
         python ./ModifySRAtoILLUMINA.py $i $i.fasta
 done
 
+# delete the original ".fastq" files
 rm ./*/*.fastq
 
+# for each of the new files, remove file extension ".fastq.fasta" and add file extension ".fasta"
 for nam in ./*/*.fastq.fasta
 do
     newname=${nam%.fastq.fasta}
